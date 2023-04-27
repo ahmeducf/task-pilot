8e2638897da16ec6f8aed6958f3f5f116c4422be
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-04-27 15:34:02 +0200

    init(build): Add webpack production configuration
      * Minify JavaScript.
    
      * Extract CSS into separate files.
    
      * Minify CSS.
    
      * Minify images.
    
      * Add output management configuration
    
      * Add code splitting configuration.

---
+++ webpack.config.prod.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+const common = require('./webpack.config.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Restaurant Page',
+        favicon: './src/assets/favicon.ico',
+        template: './src/index.html',
+        minify: {
+          removeAttributeQuotes: true,
+          collapseWhitespace: true,
+          removeComments: true,
+        },
+      }),
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              [
+                'imagemin-gifsicle',
+                { interlaced: true, quality: 20, progressive: true },
+              ],
+              ['imagemin-mozjpeg', { quality: 20, progressive: true }],
+              ['imagemin-pngquant', { quality: [0, 0.1] }],
+              ['imagemin-svgo', { plugins: [{ removeViewBox: false }] }],
+            ],
+          },
+        },
+        loader: false,
+      }),
+    ],
+  },
+});
