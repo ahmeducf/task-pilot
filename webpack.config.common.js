44acb25b36ef38dd681086653785c0b932d289e5
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-04-27 15:23:36 +0200

    init(build): Add webpack development configuration
      * Add asset management configuration.
    
      * Add output management configuration.
    
      * Add development configuration
        * Use source maps.
    
        * Use webpack-dev-server.

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -10,4 +10,7 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
---
+++ webpack.config.dev.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.config.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant Page',
+      favicon: './src/assets/favicon.ico',
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+});
