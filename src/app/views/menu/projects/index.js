43cdc8e6ade4c8c2cd521173e6f8857d9a5b16f4
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-05-08 07:09:21 +0300

    refactor: Refactor menu projects view

--- src/app/views/menu/projects/index.js
+++ src/app/views/menu/projects/index.js
@@ -1,15 +1,17 @@
 import Item from '../item';
 
-const render = (app) => {
-  const projectsContainer = document.querySelector('.projects');
-  const projectsList = projectsContainer.querySelector('.projects-list');
-
+function renderProjectsList(projectsList, app) {
   app.getProjects().forEach((project) => {
     const item = Item({ item: project });
 
     projectsList.appendChild(item);
   });
-  projectsList.style.maxHeight = `${projectsList.scrollHeight}px`;
+}
+const render = (app) => {
+  const projectsContainer = document.querySelector('.projects-container');
+  const projectsList = projectsContainer.querySelector('.projects-list');
+
+  renderProjectsList(projectsList, app);
 };
 
 export default render;
