55b68dc4c513ff20fa395c10a1aa62ff6c5fae9b
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-05-08 07:10:31 +0300

    chore: Add event listener to addProjectBtn control

--- src/app/views/menu/projects/index.js
+++ src/app/views/menu/projects/index.js
@@ -1,5 +1,10 @@
 import Item from '../item';
 
+function toggleAddProjectModal() {
+  const modal = document.querySelector('.modal-overlay.add-project');
+  modal.classList.remove('hidden');
+}
+
 function renderProjectsList(projectsList, app) {
   app.getProjects().forEach((project) => {
     const item = Item({ item: project });
@@ -10,8 +15,11 @@ function renderProjectsList(projectsList, app) {
 const render = (app) => {
   const projectsContainer = document.querySelector('.projects-container');
   const projectsList = projectsContainer.querySelector('.projects-list');
+  const addProjectBtn = projectsContainer.querySelector('.add-project-btn');
 
   renderProjectsList(projectsList, app);
+
+  addProjectBtn.addEventListener('click', toggleAddProjectModal);
 };
 
 export default render;
