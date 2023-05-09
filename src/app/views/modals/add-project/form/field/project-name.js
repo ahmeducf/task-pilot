a4762b0ef24cde44eda1647d58b4aac0cb03a6de
Ahmed Salah <ahmedsalah.yousuf@gmail.com>
2023-05-09 04:14:50 +0300

    chore: Add project name control  event listener

--- src/app/views/modals/add-project/form/field/project-name.js
+++ src/app/views/modals/add-project/form/field/project-name.js
@@ -5,6 +5,9 @@ const ProjectName = () => {
   const label = document.createElement('label');
   label.setAttribute('for', 'project-name');
   label.textContent = 'Name';
+  const charLimit = document.createElement('span');
+  charLimit.classList.add('char-limit', 'hidden');
+  label.append(charLimit);
 
   const input = document.createElement('input');
   input.setAttribute('type', 'text');
@@ -16,6 +19,26 @@ const ProjectName = () => {
   input.setAttribute('autofocus', true);
 
   formField.append(label, input);
+
+  formField.addEventListener('input', (e) => {
+    const { value } = e.target;
+    const { length } = value;
+    const { maxLength } = e.target;
+    const submitBtn = document.querySelector('.submit-btn');
+
+    if (length > 0 && length < maxLength) {
+      submitBtn.removeAttribute('disabled');
+    } else {
+      submitBtn.setAttribute('disabled', true);
+    }
+
+    if (length >= maxLength - 10 && length <= maxLength) {
+      charLimit.classList.remove('hidden');
+      charLimit.textContent = `Character limit: ${length}/${maxLength}`;
+    } else {
+      charLimit.classList.add('hidden');
+    }
+  });
 
   return formField;
 };
